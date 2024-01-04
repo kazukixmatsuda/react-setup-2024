aa05bca3b8bafdc6d45368b3b0a91ff9f377338c
kazukixmatsuda <xm.kazuki827@gmail.com>
2024-01-04 19:12:15 +0900

    eslint prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,56 @@
+/** @type {import('eslint/lib/shared/types').ConfigData} */
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended", // 追加
+    "plugin:react/jsx-runtime", // 追加
+    "plugin:react-hooks/recommended",
+    "plugin:vitest/recommended", // 追加
+    "prettier", // 追加
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "react-refresh",
+    "import", // 追加
+  ],
+  settings: {
+    react: { version: "detect" }, // 追加
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    // it ではなく test の使用を強制
+    "vitest/consistent-test-it": ["error", { fn: "test" }],
+    // import の並び順を設定
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "object",
+          "type",
+          "index",
+        ],
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: { order: "asc", caseInsensitive: true },
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+      },
+    ],
   },
-}
+};
