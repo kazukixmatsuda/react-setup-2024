462be884269ce7665165e2d3024db3dd3caa0549
kazukixmatsuda <xm.kazuki827@gmail.com>
2024-01-11 22:21:33 +0900

    テストコード修正

--- src/components/Sample.test.tsx
+++ src/components/Sample.test.tsx
@@ -5,10 +5,11 @@ import { expect, test } from "vitest";
 import Sample from "./Sample";
 
 test("サンプルテスト", async () => {
+  const user = userEvent.setup();
   render(<Sample />);
   expect(screen.getByRole("heading")).toHaveTextContent("Hello, Anonymous");
   const button = screen.getByRole("button", { name: "ユーザー名取得" });
-  userEvent.click(button);
+  user.click(button);
   expect(await screen.findByText("Loading...")).toBeInTheDocument();
   expect(await screen.findByRole("heading")).toHaveTextContent(
     "Hello, John Maverick",
